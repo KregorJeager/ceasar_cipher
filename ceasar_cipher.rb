813ff9420a49e2cb07ee8fc7ca0aa9807de3e73d
kregor <kregorjeager@gmail.com>
2025-11-22 22:24:59 +0800

    create #shift with basic functionality

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -6,4 +6,12 @@ class CeasarCipher
   def initialize
     @message = ''
   end
+
+  def shift(string, num)
+    arr_string = string.split('')
+    arr_string.map! { |letter| (letter.ord + num).chr }
+    arr_string.join('')
+  end
 end
+
+CeasarCipher.new.shift('hello', 4)

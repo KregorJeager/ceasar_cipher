5d115a24b4c1a889260e85965f77c256abcdab49
kregor <kregorjeager@gmail.com>
2025-11-22 21:59:55 +0800

    create CeasarCipher class

---
+++ ceasar_cipher.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: false
+
+# Ceasar cipher is an encryption method that shift letter in thee alpahbet.
+# Will shift all letters in a string.
+class CeasarCipher
+  def initialize
+    @message = ''
+  end
+end

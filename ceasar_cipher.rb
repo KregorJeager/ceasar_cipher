b91d92e186022aaa83dffbb16455963b3d2993fa
kregor <kregorjeager@gmail.com>
2025-11-23 14:51:26 +0800

    Add comments

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -14,8 +14,12 @@ class CeasarCipher
   end
 
   def shift_letter(ascii, shift_num)
+    # filters letter only
     if ascii > 64 && ascii < 91 || ascii > 96 && ascii < 123
+      # Find the starting number where the small and capital letter starts
       base = ascii < 90 ? 65 : 97
+      # Get the postion on the letter in the 26 letter by subtracting base
+      # shift letter in that range then add base again
       ascii = ((ascii - base + shift_num) % 26).abs + base
     end
     ascii

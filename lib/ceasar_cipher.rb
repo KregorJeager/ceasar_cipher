dd6881edadb23c98d94d3eb10060203a29de659d
kregor <kregorjeager@gmail.com>
2025-11-23 16:24:11 +0800

    Add test #ceasar_cipher

--- lib/ceasar_cipher.rb
+++ lib/ceasar_cipher.rb
@@ -26,4 +26,4 @@ class CeasarCipher
   end
 end
 
-p CeasarCipher.new.ceasar_cipher('What a string!', 5)
+CeasarCipher.new.ceasar_cipher('What a string!', 5)
--- spec/ceasar_cipher_spec.rb
+++ spec/ceasar_cipher_spec.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: false
 
 require './lib/ceasar_cipher'
+
+describe CeasarCipher do
+  subject(:cipher) { described_class.new }
+
+  describe '#ceaser_ciper' do
+    before do
+      allow(cipher).to receive(:shift_letter).and_return(66, 109, 102, 121)
+    end
+
+    context 'When given postive numbers' do
+      it 'When shifted by 5' do
+        message = 'What'
+        encrypted = cipher.ceasar_cipher(message, 5)
+        expect(encrypted).to eq('Bmfy')
+      end
+    end
+  end
+end
